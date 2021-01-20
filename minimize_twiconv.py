a77cedb118bcef5b5bd620fe56afcf76d3fbcb46
Alexander Koch <kochalexander@gmx.net>
2021-01-20 16:14:40 +0100

    Fix minimization script

--- minimize_twiconv.py
+++ minimize_twiconv.py
@@ -196,16 +196,14 @@ def minimize_partition(name, language, extension, labels, stats):
   print("Wrote {} documents to {}".format(count, output_path))
 
 def minimize_language(language, labels, stats):
-  minimize_partition("dev", language, "v4_gold_conll", labels, stats)
-  minimize_partition("train", language, "v4_gold_conll", labels, stats)
-  minimize_partition("test", language, "v4_gold_conll", labels, stats)
+  minimize_partition("dev", language, "v9_gold_conll", labels, stats)
+  minimize_partition("train", language, "v9_gold_conll", labels, stats)
+  minimize_partition("test", language, "v9_gold_conll", labels, stats)
 
 if __name__ == "__main__":
   labels = collections.defaultdict(set)
   stats = collections.defaultdict(int)
   minimize_language("english", labels, stats)
-  minimize_language("chinese", labels, stats)
-  minimize_language("arabic", labels, stats)
   for k, v in labels.items():
     print("{} = [{}]".format(k, ", ".join("\"{}\"".format(label) for label in v)))
   for k, v in stats.items():
