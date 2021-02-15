cd8035f480018f43d2427751cdc5a503ee429af6
rafi <rafiderberliner@googlemail.com>
2021-02-15 18:34:22 +0100

    Update instructions and accept epochs as parameter for probe

--- setup_training_span_extraction_per_config.sh
+++ setup_training_span_extraction_per_config.sh
@@ -10,13 +10,16 @@ output_dim=$3
 #SHould be 512 for the complete ontonotes and 32 for twiconv only
 batch_size=$4
 
+#50 epochs for all ontonotes, 10 for twiconv only
+epochs=$5
+
 echo 'extract the embeddings for all splits'
-for split in train test dev
-do
-    python span_util.py $config_name $split
-    python extract_span.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split} SPAN
-    python extract_span_predict.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split}/SPAN_PR
-done
+#for split in train test dev
+#do
+#    python span_util.py $config_name $split
+#    python extract_span.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split} SPAN
+#    python extract_span_predict.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split}/SPAN_PR
+#done
 
 
 echo 'Run the probe training with all ablations'
@@ -35,28 +38,28 @@ fi
 test_data=data/${config_name}/test/SPAN_1.h5
 exp_name=data/${config_name}/results_all_features
 
-python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}
+python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}_e${epochs} --batch_size ${batch_size} --epochs ${epochs}
 
 
 exp_name=data/${config_name}/results_ablate_boundary
 option='--ablate_boundary'
-python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size} ${option} --output_dim ${output_dim}
+python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}_e${epochs} --batch_size ${batch_size} --epochs ${epochs} ${option} --output_dim ${output_dim}
 
 exp_name=data/${config_name}/results_ablate_attention
 option='--ablate_attention'
-python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size} ${option} --output_dim ${output_dim}
+python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}_e${epochs} --batch_size ${batch_size} --epochs ${epochs} ${option} --output_dim ${output_dim}
 
 exp_name=data/${config_name}/results_ablate_span_width
 option='--ablate_span_width'
-python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size} ${option} --output_dim ${output_dim}
+python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}_e${epochs} --batch_size ${batch_size} --epochs ${epochs} ${option} --output_dim ${output_dim}
 
 exp_name=data/${config_name}/results_random
 option='--random'
-python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size} ${option} --output_dim ${output_dim}
+python train_probe.py --train_data $train_data $val_arg --test_data $test_data --exp_name ${exp_name}_d${output_dim}_bs${batch_size}_e${epochs} --batch_size ${batch_size} --epochs ${epochs} ${option} --output_dim ${output_dim}
 
 
 echo 'Outputs the error pairs for qualitative analysis'
-model_name=data/${config_name}/results_all_features_d${output_dim}_bs${batch_size}.h5
+model_name=data/${config_name}/results_all_features_d${output_dim}_bs${batch_size}_e${epochs}.h5
 test_data=data/${config_name}/test/SPAN_PR
 exp_name=data/${config_name}/SPAN_PR_AN
 python predict_joshi.py --model ${model_name} --test_data ${test_data} --exp_name ${exp_name}
