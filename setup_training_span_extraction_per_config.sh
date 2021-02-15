1752de0cf9cd87d10e04fcf085b594b56a0c98cd
rafi <rafiderberliner@googlemail.com>
2021-02-15 09:33:17 +0100

    Revert change back

--- setup_training_span_extraction_per_config.sh
+++ setup_training_span_extraction_per_config.sh
@@ -5,6 +5,13 @@ config_name=$1
 #either empty or "no_wb."
 prefix=$2
 
+echo 'extract the embeddings for all splits'
+for split in train test dev
+do
+    python span_util.py $config_name $split
+    python extract_span.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split} SPAN
+    python extract_span_predict.py $config_name NP.${prefix}${split}.english.jsonlines data/${config_name}/${split}/SPAN_PR
+done
 
 
 echo 'Run the probe training with all ablations'
